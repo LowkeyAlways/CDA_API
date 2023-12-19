2565355bcc51c22784f07c6786f994e7798a89ff
AndyLS <50944069+AndyL94@users.noreply.github.com>
2023-12-19 14:25:40 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -6,24 +6,32 @@ document.addEventListener("DOMContentLoaded",e =>{
     xmlhttp.send();
     xmlDoc = xmlhttp.responseXML; 
     let produits = xmlDoc.getElementsByTagName("produit");
-  let vProduits = "<ul>";
-  for (let i = 0; i < produits.length; i++) {
+    let vProduits = "";
+    for (let i = 0; i < produits.length; i++) {
     let image = produits[i].getElementsByTagName("image")[0].childNodes[0].nodeValue;
     let nom = produits[i].getElementsByTagName("nom")[0].childNodes[0].nodeValue;
     let mandat = produits[i].getElementsByTagName("mandat")[0].childNodes[0].nodeValue;
     let naissance = produits[i].getElementsByTagName("naissance")[0].childNodes[0].nodeValue;
     let statut = produits[i].getElementsByTagName("statut")[0].childNodes[0].nodeValue;
     
-
-    vProduits += "<li><img src='" + image + "'></li>";
-    vProduits += "<li>Nom : " + nom + "</li>";
-    vProduits += "<li>Mandat : " + mandat + "</li>";
-    vProduits += "<li>Naissance : " + naissance + "</li>";
-    vProduits += "<li>Statut : " + statut + "</li>";
-    vProduits += "</ul>";
-    vProduits += "<ul>";
+    vProduits += `<figure><img src="${image}" alt="${nom}">
+                    <figcaption>
+                      <ul>
+                        <li>Nom : ${nom}</li>
+                        <li>Mandat : ${mandat}</li>
+                        <li>Naissance : ${naissance}</li>
+                        <li>Statut : ${statut}</li>
+                      </ul>
+                    </figcaption>
+                  </figure>
+                  `;
     
   }
   
   document.getElementById("produits").innerHTML = vProduits;
+
+// Récupère l'année courante
+const currentYear = new Date().getFullYear();
+// Met à jour le contenu du span
+document.querySelector("footer p").textContent = currentYear + " - Lowkey";
 });
